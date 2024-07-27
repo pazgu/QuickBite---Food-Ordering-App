a4ca5c3eb64fe8d6fb3ed35be41235c66a2d3c06
Paz Gueta <pazgueta@gmail.com>
2024-07-27 12:44:37 +0300

    handle validations and errors to formdata of user details

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { userController } from "../controllers/userController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 const { createCurrentUser, updateCurrentUser } = userController;
 
 router.post("/", jwtCheck, createCurrentUser);
-router.put("/", jwtCheck, jwtParse, updateCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, updateCurrentUser);
 
 export default router;
